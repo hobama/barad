fe19cff15e77bbd356708e39706b266c07f67e4c
ckillmar <ckillmar@af16c015-b033-0410-a596-71a6d81f68ef>
2007-07-08 06:03:08 +0000

    Initial commit.

---
+++ trunk/BaradAgent/utils.h
@@ -0,0 +1,52 @@
+/*
+ * University of Texas at Austin
+ * Barad Project, Jul 1, 2007
+ */
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "exceptions.h"
+#include "windows.h"
+
+#include <string>
+#include <jvmti.h>
+
+namespace barad {
+	using std::string;
+
+	class Utils {
+	private:
+		static string BARAD_HOME_VARIABLE_NAME;
+
+	public:
+		/** 
+		 * \brief Logs an error and exits.
+		 */
+		static void fatalError(const string& message, int exitCode = 1);
+
+		/**
+		 * \brief Every JVMTI interface returns an error code, which should be checked to avoid any cascading errors down the line.
+		 * The interface GetErrorName() returns the actual enumeration constant
+		 * name, making the error messages much easier to understand.
+		 */
+		static void checkJVMTIError(jvmtiEnv* jvmti, jvmtiError error, const string& message = "");
+
+		/**
+		 * \brief All memory allocated by JVMTI must be freed by the JVMTI Deallocate interface.
+		 */
+		static void deallocate(jvmtiEnv* jvmti, void* ptr);
+
+		/**
+		 * \brief Allocation of JVMTI managed memory.
+		 */
+		static void* allocate(jvmtiEnv* jvmti, jint len);		
+
+		static void addFileToBootClasspath(jvmtiEnv* jvmti, const string& fileName);
+	private:
+		Utils() {}
+
+		static string getBaradHome();
+	};
+}
+
+#endif UTILS_H
