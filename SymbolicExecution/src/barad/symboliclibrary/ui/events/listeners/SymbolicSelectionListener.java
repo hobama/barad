48ec52321d5ca0ca92560a6b7364901f86d8e8a7
svetoslavganov <svetoslavganov@af16c015-b033-0410-a596-71a6d81f68ef>
2008-01-12 21:00:19 +0000

    SVN complains will re check in fresh

--- SymbolicExecution/src/barad/symboliclibrary/ui/events/listeners/SymbolicSelectionListener.java
+++
@@ -1,8 +0,0 @@
-package barad.symboliclibrary.ui.events.listeners;
-
-import barad.symboliclibrary.ui.events.SymbolicSelectionEvent;
-
-public interface SymbolicSelectionListener {
-	public void widgetSelected(SymbolicSelectionEvent event);
-	public void widgetDefaultSelected(SymbolicSelectionEvent event);
-}
